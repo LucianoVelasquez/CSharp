ffdfc809c7649cf5b4516447c7afe1860be01445
LucianoVelasquez <125805986+LucianoVelasquez@users.noreply.github.com>
2023-09-26 09:07:45 -0300

    add new activities

---
+++ CsharpProjects/Arrays/Program.cs
@@ -0,0 +1,43 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] pallets = { "B14", "A11", "B12", "A13" };
+        Array.Sort(pallets);
+        //Ordenando de mayor a menor y de A - Z
+        foreach(string pallet in pallets)
+        {
+            Console.WriteLine(pallet);
+        }
+        Console.WriteLine("\n invertir");
+        //invertir
+        Console.WriteLine("\n");
+        Array.Reverse(pallets);
+        foreach (string pallet in pallets)
+        {
+            Console.WriteLine(pallet);
+        }
+        Console.WriteLine("\n Vacia elementos del array");
+        //Vacia elementos del array
+        Array.Clear(pallets, 0, 3);
+        foreach (string pallet in pallets)
+        {
+            if (pallet == null)
+            {
+                Console.WriteLine("No estoy apuntando a ninguna parte");
+            }
+            Console.WriteLine(pallet);
+        }
+        Console.WriteLine("\n Aumentando el tamanio del array.");
+        //Aumentando el tamanio del array.
+        Array.Resize(ref pallets, 6);
+        foreach (string pallet in pallets)
+        {
+            if(pallet == null)
+            {
+                Console.WriteLine("No estoy apuntando a ninguna parte");
+            }
+            Console.WriteLine(pallet);
+        }
+    }
+}
