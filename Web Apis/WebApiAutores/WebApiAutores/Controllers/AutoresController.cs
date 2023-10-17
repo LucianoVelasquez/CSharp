271f566a1c87e00a148da7602e5e379db588dd27
LucianoVelasquez <125805986+LucianoVelasquez@users.noreply.github.com>
2023-10-17 10:44:47 -0300

    add simple web api

---
+++ Web Apis/WebApiAutores/WebApiAutores/Controllers/AutoresController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApiAutores.Entidades;
+
+namespace WebApiAutores.Controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresController: ControllerBase
+    {
+        private readonly AplicationDbContext context;
+
+        public AutoresController(AplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Autor>>> Get()
+        {
+            return await context.Autors.Include(x => x.Libros).ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Autor autor)
+        {
+            context.Add(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut("{id:int}")] // api/autores/id
+        public async Task<ActionResult> Put(Autor autor, int id)
+        {
+            if(autor.Id != id)
+            {
+                return BadRequest("El id del autor no coincide con el id de la URL");
+            }
+
+            context.Update(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var existe = await context.Autors.AnyAsync(x => x.Id == id);
+
+            if (!existe) return NotFound();
+
+            context.Remove(new Autor { Id = id });
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
