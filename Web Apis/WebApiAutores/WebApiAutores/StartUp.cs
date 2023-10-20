7e2a067af53bf7d6132208a8bf6e8170d507ea35
LucianoVelasquez <125805986+LucianoVelasquez@users.noreply.github.com>
2023-10-20 09:52:49 -0300

    add fixed

--- Web Apis/WebApiAutores/WebApiAutores/StartUp.cs
+++ Web Apis/WebApiAutores/WebApiAutores/StartUp.cs
@@ -12,12 +12,14 @@ namespace WebApiAutores
 
         public IConfiguration Configuration { get; }
 
-        public void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services) //Sistema de inyenccion de dependencia.
         {
             services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
             services.AddDbContext<AplicationDbContext>(options => 
                     options.UseSqlServer(Configuration.GetConnectionString("defaultConnection")));
+
+           
 
             services.AddEndpointsApiExplorer();
 
@@ -25,6 +27,17 @@ namespace WebApiAutores
         }
         public void Configure(IApplicationBuilder app,IWebHostEnvironment env)
         {
+            //MiddleWares:
+
+            app.Map("/ruta1", app => //Ejemplo de Middleware
+            {
+                app.Run(async contexto =>
+                {
+                    await contexto.Response.WriteAsync("Soy un middleware.");
+                });
+            });
+            
+
             if (env.IsDevelopment())
             {
                 app.UseSwagger();
