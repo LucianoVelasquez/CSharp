3397d14d6679d42eb19ccd790dece0970bb4728d
LucianoVelasquez <125805986+LucianoVelasquez@users.noreply.github.com>
2023-11-07 16:35:46 -0300

    Add config cors

---
+++ Web Apis/WebApiAutores/WenApiAutores.Test/PruebasUnitarias/MaxCaracteres.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace WenApiAutores.Test.PruebasUnitarias
+{
+    [TestClass]
+    public class MaxCaracteresTests
+    {
+        [TestMethod]
+        public void LongitudNombreMayorACuatro_DevuelveError()
+        {
+        
+        }
+
+       
+    }
+}
